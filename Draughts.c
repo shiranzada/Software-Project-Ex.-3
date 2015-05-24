0c9efab442010ca48ac28c54cd5dbfbb96976737
renenperlman <renenp@gmail.com>
2015-05-24 17:22:37 +0300

    Draughts files

---
+++ Draughts.c
@@ -0,0 +1,66 @@
+#include "Draughts.h"
+
+int main()
+{
+	char board[BOARD_SIZE][BOARD_SIZE];
+	init_board(board);
+	print_board(board);
+	print_message(WRONG_MINIMAX_DEPTH);
+	perror_message("TEST");
+	return 0;
+}
+
+
+void print_line(){
+	int i;
+	printf("  |");
+	for (i = 1; i < BOARD_SIZE*4; i++){
+		printf("-");
+	}
+	printf("|\n");
+}
+
+void print_board(char board[BOARD_SIZE][BOARD_SIZE])
+{
+	int i,j;
+	print_line();
+	for (j = BOARD_SIZE-1; j >= 0 ; j--)
+	{
+		printf((j < 9 ? " %d" : "%d"), j+1);
+		for (i = 0; i < BOARD_SIZE; i++){
+			printf("| %c ", board[i][j]);
+		}
+		printf("|\n");
+		print_line();
+	}
+	printf("   ");
+	for (j = 0; j < BOARD_SIZE; j++){
+		printf(" %c  ", (char)('a' + j));
+	}
+	printf("\n");
+}
+
+
+
+void init_board(char board[BOARD_SIZE][BOARD_SIZE]){
+	int i,j;
+	for (i = 0; i < BOARD_SIZE; i++){
+		for (j = 0; j < BOARD_SIZE; j++){
+			if ((i + j) % 2 == 0){
+				if (j <= 3){
+					board[i][j] = WHITE_M;
+				}
+				else if (j >= 6){
+					board[i][j] = BLACK_M;
+				}
+				else{
+					board[i][j] = EMPTY;
+				}
+			}
+			else{
+				board[i][j] = EMPTY;
+			}
+		}
+	}
+}
+
