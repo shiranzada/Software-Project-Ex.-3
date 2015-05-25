56cde57937d37d021d36a3115ce4313f2abe0954
renenperlman <renenp@gmail.com>
2015-05-25 12:22:31 +0300

    Update Draughts.h

--- Draughts.h
+++ Draughts.h
@@ -1,111 +1,92 @@
-#include "Draughts.h"
-#include <limits.h>
+#ifndef DRAUGHTS_
+#define DRAUGHTS_
 
-int main()
+#include<stdio.h>
+
+
+#define WHITE_M 'm'
+#define WHITE_K 'k'
+#define BLACK_M 'M'
+#define BLACK_K 'K'
+#define EMPTY ' '
+
+#define BOARD_SIZE 10
+
+typedef char** board_t;
+#define WELCOME_TO_DRAUGHTS "Welcome to Draughts!\n"
+#define ENTER_SETTINGS "Enter game settings:\n" 
+#define WRONG_MINIMAX_DEPTH "Wrong value for minimax depth. The value should be between 1 to 6\n"
+#define WRONG_POSITION "Invalid position on the board\n"
+#define NO_DICS "The specified position does not contain your piece\n"
+#define ILLEGAL_COMMAND "Illegal command, please try again\n"
+#define ILLEGAL_MOVE "Illegal move\n"
+#define WROND_BOARD_INITIALIZATION "Wrong board initialization\n"
+ 
+#define ENTER_YOUR_MOVE "Enter your move:\n" 
+#define perror_message(func_name) (fprintf(stderr, "Error: standard function %s has failed\n", func_name))
+#define print_message(message) (printf("%s", message));
+
+
+
+void print_board(char board[BOARD_SIZE][BOARD_SIZE]);
+void init_board(char board[BOARD_SIZE][BOARD_SIZE]);
+
+struct listNode
 {
-	init_board(board);
-	print_board(board);
-	print_message(WRONG_MINIMAX_DEPTH);
-	perror_message("TEST");
-	return 0;
-}
+	struct listNode* next;
+	void* data;
+};
 
-
-void print_line(){
-	int i;
-	printf("  |");
-	for (i = 1; i < BOARD_SIZE*4; i++){
-		printf("-");
-	}
-	printf("|\n");
-}
-
-void print_board(char board[BOARD_SIZE][BOARD_SIZE])
+struct linkedList
 {
-	int i,j;
-	print_line();
-	for (j = BOARD_SIZE-1; j >= 0 ; j--)
-	{
-		printf((j < 9 ? " %d" : "%d"), j+1);
-		for (i = 0; i < BOARD_SIZE; i++){
-			printf("| %c ", board[i][j]);
-		}
-		printf("|\n");
-		print_line();
-	}
-	printf("   ");
-	for (j = 0; j < BOARD_SIZE; j++){
-		printf(" %c  ", (char)('a' + j));
-	}
-	printf("\n");
-}
+	struct listNode* first;
+};
+
+struct location
+{
+	char x;
+	int y;
+};
+
+struct steps
+{
+	linkedList listOfSteps;
+	listNode* last;
+	int numberOfEats;
+};
 
 
 
-void init_board(char board[BOARD_SIZE][BOARD_SIZE]){
-	int i,j;
-	for (i = 0; i < BOARD_SIZE; i++){
-		for (j = 0; j < BOARD_SIZE; j++){
-			if ((i + j) % 2 == 0){
-				if (j <= 3){
-					board[i][j] = WHITE_M;
-				}
-				else if (j >= 6){
-					board[i][j] = BLACK_M;
-				}
-				else{
-					board[i][j] = EMPTY;
-				}
-			}
-			else{
-				board[i][j] = EMPTY;
-			}
-		}
-	}
-}
+typedef struct linkedList linkedList;
+typedef struct listNode listNode;
+typedef struct location location;
+typedef struct steps steps;
+linkedList moves = { NULL }; // the global list of all possible moves
+int minmaxDepth = 1;
+char userColor = 'w';
+char board[BOARD_SIZE][BOARD_SIZE];
+void mMoveStep(board_t, char, location, steps); // adds to the list of steps any possible one step move of a man
+void kMoveStep(board_t, char, location, steps);// adds to the list of steps any possible one step move of a king
+void moveEat(board_t, char, location, steps); // adds to the list of steps any possible one eat
 
-int max(int depth, char player, board_t board, steps** bestStep){
-	if (depth == 0){
-		return score(board);
-	}
-	char otherPlayer = ('b' + 'w' - player);
-	int currMin = INT_MAX;
-	int stepScore;
-	linkedList moves = setMoveList(otherPlayer, board);
-	listNode* node = moves.first;
-	while (node!=NULL)
-	{
-		stepScore = score(min(depth - 1, otherPlayer, moveDisc(*(steps*)node->data, player)));
-		if (stepScore < currMin){
-			currMin = stepScore;
-			*bestStep = (steps*)node->data;
-		}
-	}
-	return currMin;
-}
+char* getInput(FILE * , size_t);
+void analysis(char*);
+void setMinmaxDepth(int); // checks if 1<=int<=6
+void setUserColor(char*); // checks if color == white || black
+void clear();
+void removeDisc(location); // checks if legal location (1<=x<=10 'a'<=y<='j')
+void setDisc(location, char*, char); // checks if legal location (1<=x<=10 'a'<=y<='j')
+void quit(); // frees all mallocs
+void start(); // checks if 1. board is empty 2. only one color 3. more than 20 discs in the same color
+int isLegalPosition(location);
+board_t moveDisc(steps, char);
+linkedList setMoveList(char, board_t);
+void printMoveList();
+int isLegalMove(steps);
+int compateLoc(location, location);
+int max(int , char , board_t , steps* );
+int min(int , char , board_t , steps* );
 
-int min(int depth, char player, board_t board, steps** bestStep){
-	if (depth == 0){
-		return score(board);
-	}
-	char otherPlayer = ('b' + 'w' - player);
-	int currMin = INT_MIN;
-	int stepScore;
-	linkedList moves = setMoveList(otherPlayer, board);
-	listNode* node = moves.first;
-	while (node != NULL)
-	{
-		stepScore = score(max(depth - 1, otherPlayer, moveDisc(*(steps*)node->data, player)));
-		if (stepScore > currMin){
-			currMin = stepScore;
-			*bestStep = (steps*)node->data;
-		}
-	}
-	return currMin;
-}
+#endif  
 
-steps* minmax(char player){
-	steps* bestStep = NULL;
-	max(minmaxDepth, player, board, &bestStep)
-}
 
